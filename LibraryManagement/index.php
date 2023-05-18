b7e29ff0e68cd463bf74bdb07221dcdbf0e359bc
s25770 <s25770@pjwstk.edu.pl>
2023-05-18 18:48:22 +0200

    .

---
+++ LibraryManagement/index.php
@@ -0,0 +1,39 @@
+<?php
+
+	session_start();
+	
+	if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true))
+	{
+		header('Location: ksiegarnia.php');
+		exit();
+	}
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Księgarnia internetowa</title>
+</head>
+
+<body>
+	
+	<a href="rejestracja.php">Rejestracja - załóż darmowe konto!</a>
+	<br /><br />
+	
+	<form action="zaloguj.php" method="post">
+	
+		Login: <br /> <input type="text" name="login" /> <br />
+		Hasło: <br /> <input type="password" name="haslo" /> <br /><br />
+		<input type="submit" value="Zaloguj się" />
+	
+	</form>
+	
+<?php
+	if(isset($_SESSION['blad']))	echo $_SESSION['blad'];
+?>
+
+</body>
+</html>
