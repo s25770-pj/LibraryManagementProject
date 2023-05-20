4f7ca4ea88eef7433f425522cde1d3be0a7692d9
s25770 <s25770@pjwstk.edu.pl>
2023-05-20 18:17:44 +0200

    v0.012

--- LibraryManagement/Panel_Uzytkownika/userpanel.php
+++ LibraryManagement/Panel_Uzytkownika/userpanel.php
@@ -25,17 +25,16 @@
 
 $dataczas = new DateTime(date('Y-m-d H:i:s'));
 
-echo "Data i czas serwera: " . $dataczas->format('Y-m-d H:i:s') . "<br>";
-
 $koniec = DateTime::createFromFormat('Y-m-d H:i:s', $_SESSION['dnipremium']);
 
 $roznica = $dataczas->diff($koniec);
 
 if($dataczas<$koniec) {
-	echo "Pozostałe dni premium: " . $roznica->format('%y lat, %m, mies, %d dni, %h godz, %i min, %s sek');
+	echo "Pakiet premium aktywny do: <br />" . $_SESSION['dnipremium'];
 } else {
-	echo "Premium nieaktywne od: " . $roznica->format('%y lat, %m, mies, %d dni, %h godz, %i min, %s sek');
+	echo "Premium nieaktywne.";
 }
+
 	echo "<p>Witaj ".$_SESSION['user'].'! [ <a href="../Logowanie_Rejestracja/logout.php">Wyloguj się!</a> ]</p>';
 	echo "<p><b>E-mail</b>: ".$_SESSION['email']. "</b></p>";
 
