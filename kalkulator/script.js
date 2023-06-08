2b0762d8fab4b0da5cc10abd2817c14d50e98d9c
s25770 <s25770@pjwstk.edu.pl>
2023-06-08 16:46:10 +0200

    .

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -1,35 +1,36 @@
-// var numbers = document.getElementsByClassName('number');
-// document.querySelectorAll('.number');
-// console.log(numbers);
-// [...numbers].forEach(el=> {
-//     console.log(el,"element");
-//  });
+var numbers = document.getElementsByClassName('number');
+document.querySelectorAll('.number');
+console.log(numbers);
+[...numbers].forEach(el=> {
+    console.log(el,"element");
+ });
 
 
-// [...numbers].forEach(el=> {
-//         console.log(el,"element");
-// });
-
-
-num = [1,2,3];
-acc = "przyklad";
-
-num.forEach(el=> {
-    el=el*4+2;
+[...numbers].forEach(el=> {
+        el = el+1;
+        console.log(numbers);
 });
 
-console.log(num);
+
+// num = [1,2,3];
+// acc = "przyklad";
+
+// num.forEach(el=> {
+//     el=el*4+2;
+// });
+
+// console.log(num);
 
 
-var numMap = num.map(el=> el*2);
+// var numMap = num.map(el=> el*2);
 
-console.log(numMap);
+// console.log(numMap);
 
-var filter = num.filter(el=> el%2 == 0 );
+// var filter = num.filter(el=> el%2 == 0 );
 
-console.log(filter);
+// console.log(filter);
 
-var reduceEx = num.reduce((a,b)=>a+b,5);
+// var reduceEx = num.reduce((a,b)=>a+b,5);
 
-var reduceExS = [...acc].reduce((acc, curr) => (acc[curr] = (acc[curr] || 0) + 1, acc), {});
-console.log(reduceExS);
+// var reduceExS = [...acc].reduce((acc, curr) => (acc[curr] = (acc[curr] || 0) + 1, acc), {});
+// console.log(reduceExS);
