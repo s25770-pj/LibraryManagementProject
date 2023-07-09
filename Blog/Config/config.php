2b4bfb31b0db795bad9fa51ea33e3de7357091eb
s25770-pj <92208994+s25770-pj@users.noreply.github.com>
2023-07-10 00:51:27 +0200

    Delete Blog directory

--- Blog/Config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'database.php';
-require_once 'path.php';
-$style ='<link rel="stylesheet" type="text/css" href="../CSS/style.css">';
--- Blog/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['logged'])){
-header("Location: Service/blog.php");
-} else {
-    header("Location: Login/login_page.php");
-}
